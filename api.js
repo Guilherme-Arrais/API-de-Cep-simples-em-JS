62308d8d57526584bf3a723b70f7f764dfb7481b
Guilherme Arrais <114041284+Guilherme-Arrais@users.noreply.github.com>
2023-07-20 21:03:00 -0300

    2.5v

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const axios = require('axios').default
 const { error } = require('console')
 const fs = require('fs')
 
-const cep = 01452002
+const cep = 11704450
 const viacep=(async()=>{
     const {data}= await axios(`https://viacep.com.br/ws/${cep}/json`) 
       fs.writeFile('local.json', JSON.stringify(data, null, 2), err =>{
